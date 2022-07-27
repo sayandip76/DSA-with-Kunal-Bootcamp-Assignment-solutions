1d462fe8cacedce811b57fa6a3cd7585110081b3
Sayandip Kar <sayandeepkar53@gmail.com>
2022-07-27 08:42:23 +0530

    Calculating the sum of negative, positive even and odd numbers separately

---
+++ src/com/sayan/conditionals_loops/intermediate_java_programs/sum_negative_even_odd.java
@@ -0,0 +1,31 @@
+package com.sayan.conditionals_loops.intermediate_java_programs;
+
+import java.util.Scanner;
+
+public class sum_negative_even_odd {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        int num,sum_neg=0,sum_e=0,sum_o=0;
+        boolean in=true;
+        System.out.println("Enter the no. one by one and end the process with 0: ");
+        while (in){
+            num= inp.nextInt();
+            if (num==0)
+                in=false;
+            if (num<0){
+                sum_neg+=num*(-1);
+            }
+            else {
+                if (num%2==0){
+                    sum_e+=num;
+                }
+                else {
+                    sum_o+=num;
+                }
+            }
+        }
+        System.out.println("Sum of -Ve no.: -"+sum_neg);
+        System.out.println("Sum of +Ve even no.: "+sum_e);
+        System.out.println("Sum of +Ve odd no.: "+sum_o);
+    }
+}
