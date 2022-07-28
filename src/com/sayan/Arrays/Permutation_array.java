cd860a4fe3d24ad8e267f2ca11d15a57a0e7a262
Sayandip Kar <sayandeepkar53@gmail.com>
2022-07-28 14:45:14 +0530

    Array assignment Leetcode question 1

---
+++ src/com/sayan/Arrays/Permutation_array.java
@@ -0,0 +1,17 @@
+package com.sayan.Arrays;
+
+import java.util.*;
+
+public class Permutation_array {
+    public static void main(String[] args) {
+        int[] arr={0,2,1,5,3,4};
+        System.out.println(Arrays.toString(buildArray(arr)));
+    }
+    public static int[] buildArray(int[] nums) {
+        int[] ans=new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            ans[i]=nums[nums[i]];
+        }
+        return (ans);
+    }
+}
