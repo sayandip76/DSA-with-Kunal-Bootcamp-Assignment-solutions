7374f257cdae2a63c03ae85c8916d93faf568373
Sayandip Kar <sayandeepkar53@gmail.com>
2022-07-28 14:58:28 +0530

    Arrays assignment Leetcode question 2: Concatenation of arrays

---
+++ src/com/sayan/Arrays/Concatenation_arrays.java
@@ -0,0 +1,19 @@
+package com.sayan.Arrays;
+
+import java.util.Arrays;
+
+public class Concatenation_arrays {
+    public static void main(String[] args) {
+        int[] arr={1,2,1};
+        System.out.println(Arrays.toString(getConcatenation(arr)));
+    }
+    public static int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[2*n];
+        for(int i=0; i<n; i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+        }
+        return ans;
+    }
+}
